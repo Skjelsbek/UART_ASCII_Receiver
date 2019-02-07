59c3a3cd585a9abb6261a5a4fda489b00a6ab22c
Jon Skjelsbæk <skjelsbek@hotmail.com>
2019-02-07 21:25:03 +0100

    Init repo

---
+++ UART_ASCII_Receiver.srcs/sim_1/new/UART_tb.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--use IEEE.NUMERIC_STD.ALL;
+
+entity UART_tb is
+--  Port ( );
+end UART_tb;
+
+architecture Behavioral of UART_tb is
+
+begin
+
+
+end Behavioral;
